868c8f678ffd1e5092cd008b7c851356d3c5437b
Avinash <optevus510@gmail.com>
2023-05-19 17:14:14 +0530

    checkware program

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {Component} from '@angular/core';
+
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+ 
+import {MatToolbarModule} from '@angular/material/toolbar';import {MatMenuModule} from '@angular/material/menu';  
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    BrowserAnimationsModule,
+  MatIconModule, 
+    MatToolbarModule,
+    MatMenuModule,
+    Component
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
